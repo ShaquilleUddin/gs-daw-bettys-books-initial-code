a36c09cc7ca4f0a589b41e1c35b7f593ec1123c0
Shaquille <119819715+ShaquilleUddin@users.noreply.github.com>
2024-10-15 12:55:52 +0100

    Lab 1 + Lab 2
    All tasks in Lab 1 and Task 1 + Task 2 of Lab 3

--- create_db.sql
+++ create_db.sql
@@ -4,8 +4,11 @@
 CREATE DATABASE IF NOT EXISTS bettys_books;
 USE bettys_books;
 
-# Create the tables
+# Create the tables for books 
 CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
+
+# Create the tables for users
+CREATE TABLE IF NOT EXISTS users (id INT AUTO_INCREMENT,username VARCHAR(50),first_name VARCHAR(50),last_name VARCHAR(50),email VARCHAR(50),hashedPassword VARCHAR(100),PRIMARY KEY(id));
 
 # Create the app user
 CREATE USER IF NOT EXISTS 'bettys_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
--- routes/users.js
+++ routes/users.js
@@ -2,14 +2,47 @@
 const express = require("express")
 const router = express.Router()
 
+// Importing the bcrypt 
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
 router.get('/register', function (req, res, next) {
     res.render('register.ejs')                                                               
 })    
 
 router.post('/registered', function (req, res, next) {
-    // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email)                                                                           
-})
+    // Hashing the password
+    const plainPassword = req.body.password
+    bcrypt.hash(plainPassword, saltRounds, function(err, hashedPassword) {
+        if (err) {
+            return next(err); // Handle error during hashing
+        }
+
+        // Using SQLQuery to insert the user data into the database
+        let sqlquery = "INSERT INTO users (username, first_name, last_name, email, hashedPassword) VALUES (?, ?, ?, ?, ?)"
+        
+        // Collecting the user's data from the form and then storing it
+        let newUser = [
+            req.body.username,
+            req.body.first,
+            req.body.last,
+            req.body.email,
+            hashedPassword // Storing the hashed password
+        ]
+
+        // SQL Query will save the user data in the database
+        db.query(sqlquery, newUser, (err, result) => {
+            if (err) {
+                return next(err) // Handle the error during database insertion
+            }
+
+            // saving data in database
+            let message = 'Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email
+            message += '. Your password is: '+ req.body.password +' and your hashed password is: '+ hashedPassword
+            res.send(message)
+        });
+    });
+});
 
 // Export the router object so index.js can access it
 module.exports = router

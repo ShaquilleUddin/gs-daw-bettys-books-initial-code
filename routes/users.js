29fb225a726b9d88f0bc5dd5a5c9a4e5757f5017
Shaquille <119819715+ShaquilleUddin@users.noreply.github.com>
2024-10-15 14:12:31 +0100

    Lab 2 Complete
    Task 3 + Task 4
    Created a list for users display
    Created a functional log-in page
    Log-in page compares usernames and passwords within database and responds to success or failure of the inputted information

--- routes/users.js
+++ routes/users.js
@@ -44,5 +44,60 @@ router.post('/registered', function (req, res, next) {
     });
 });
 
+// List all the users without showing any passwords
+router.get('/list', function (req, res, next) {
+    // Get user data without the hashed passowrd 
+    let sqlquery = "SELECT username, first_name, last_name, email FROM users" // Exclude the hashedPassword
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            return next(err)
+        }
+
+        // Render the list of users using list.ejs
+        res.render('listusers.ejs', {availableUsers: result})
+    });
+});   
+
+// Log-in page for users
+router.get('/login', function (req, res, next) {
+    res.render('login.ejs'); // Render the login page using login.ejs 
+});
+
+// Log-in page for users
+router.post('/loggedin', function (req, res, next) {
+    // Find users in the database
+    let sqlquery = "SELECT hashedPassword FROM users WHERE username = ?";
+    
+    // Finding the users in the database
+    db.query(sqlquery, [req.body.username], (err, results) => {
+        if (err) {
+            return next(err); // Handle error during database query
+        }
+
+        // Checking if the user is found
+        if (results.length === 0) {
+            return res.send('Login failed: Username not found.'); // This is if the user not found
+        }
+
+        // Get the hashed password from the database
+        const hashedPassword = results[0].hashedPassword;
+
+        // Compare the hashed password with the password entered by the user
+        bcrypt.compare(req.body.password, hashedPassword, function(err, result) {
+            if (err) {
+                return next(err); // Handles any errors during the comparison between entered password and database password
+            }
+            else if (result === true) {
+                // Login was successful
+                return res.send('Login was successful! Welcome back, ' + req.body.username + '!');
+            }
+            else {
+                // Login was failed
+                return res.send('Login was failed: Your password was incorrect.');
+            }
+        });
+    });
+});
+
 // Export the router object so index.js can access it
 module.exports = router
